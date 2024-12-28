0393ae6b51b35780bd58b57c6a5a3ac6bff8ef43
Mariya Stepa <stepa@DESKTOP-KAIIJ29>
2024-12-28 12:08:19 +0100

    df

--- js/task-2.js
+++ js/task-2.js
@@ -15,5 +15,5 @@ function getShippingMessage(country, price, deliveryFree) {
 }
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credit
 
